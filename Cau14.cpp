7c8a552a606d51c1955347ca0214cfd8bb6a1a15
Ninh <phuninh0804@gmail.com>
2025-10-05 20:12:14 +0700

    Add files via upload

---
+++ Cau14.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+int n=0; cout << " Nhap n \n"; cin >> n;
+ int X=0; cout<< " Nhap X \n"; cin >> X;
+ long long s=0; long long A=0;
+for(int i=1 ;i<= n ; i++){
+    A=pow(X  ,(i*2)+1);
+    s+=A;
+
+}
+ cout << " Gia tri cua s"<< " " << s << endl;
+ return 0;
+}
