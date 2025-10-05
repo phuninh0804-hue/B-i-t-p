b39a3026c4263c70c823e51a9107fde145a05f93
Ninh <phuninh0804@gmail.com>
2025-10-05 19:50:59 +0700

    Add files via upload

---
+++ Cau11.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+int n=0; cout << " Nhap n \n"; cin >> n;
+ int X=0; cout<< " Nhap X \n"; cin >> X;
+
+ cout << " Gia tri cua T"<< " " << pow(X, n )<< endl;
+ return 0;
+}
