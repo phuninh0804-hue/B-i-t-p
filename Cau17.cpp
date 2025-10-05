f5cbd277094b66bd64bfb01fc973155b0e8df6b4
Ninh <phuninh0804@gmail.com>
2025-10-05 22:23:55 +0700

    Add files via upload

---
+++ Cau17.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+int n=0; cout << "Nhap n? \n"; cin >> n;
+int X=0; cout <<"Nhap X? \n"; cin >> X;
+double s=0; double A=1; double C=1 // A là tử , C là mẫu
+;
+for( int i=1; i<=n ; i++){
+    A *= i ; //
+    C*= X;
+s +=  C/A;
+}
+cout << "Gia tri cua s ("<< n << ")"<< s << endl;
+return 0 ;
+}
+
