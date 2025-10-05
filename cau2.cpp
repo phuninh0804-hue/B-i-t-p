87561f652ac0c4edd2ffcb24b4ddde2fa2f88262
Ninh <phuninh0804@gmail.com>
2025-10-05 14:10:11 +0700

    Add files via upload

---
+++ cau2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+long long n;
+cout << "nhap n ?\n" ;
+cin >> n;
+ long long S = n* (n + 1) * (2*n + 1) /6;
+ cout << " Tong cua S la  " << S <<endl;
+}
