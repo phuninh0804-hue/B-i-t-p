ed7024d285256be1da89b17754abd99cd27c92f7
Ninh <phuninh0804@gmail.com>
2025-10-06 18:31:51 +0700

    Update Cau16.cpp

--- Cau16.cpp
+++ Cau16.cpp
@@ -6,12 +6,13 @@ int n=0; cout << "Nhap n? \n"; cin >> n;
 int X=0; cout <<"Nhap X? \n"; cin >> X;
 double s=0; double A=0;
 for( int i=1; i<=n ; i++){
-    double  A=  (i * (i + 1)) /2; // Vì 1+2+3+...+i = i*(i+1)/2
+    A=  (i * (i + 1)) /2; // Vì 1+2+3+...+i = i*(i+1)/2
      s += pow(X , n) / A;
 }
 cout << "Gia tri cua s ("<< n <<","<< X << ")"<< s << endl;
 return 0 ;
 }
+
 
 
 
