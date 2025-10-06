002d189519ed8e32949cd03e50fbfdfef23f5490
Ninh <phuninh0804@gmail.com>
2025-10-06 18:31:22 +0700

    Update Cau18.cpp

--- Cau18.cpp
+++ Cau18.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
 int n=0; cout << "Nhap n? \n"; cin >> n;
 int X=0; cout <<"Nhap X? \n"; cin >> X;
-double s=1; double A=1; double C=1 // A là tử , C là mẫu S=1 do từ sô 1 trở đi
+double s=1; double A=1; double C=1 ;// A là tử , C là mẫu S=1 do từ sô 1 trở đi
 ;
 for( int i=1; i<=n ; i++){
     A *=(2 * i - 1) * (2 * i) ; //
@@ -15,4 +15,5 @@ s +=  C/A;
 cout << "Gia tri cua s ("<< n << ")"<< s << endl;
 return 0 ;
 }
+
 
