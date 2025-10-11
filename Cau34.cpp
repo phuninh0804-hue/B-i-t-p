fc5090f8b601716b073a14cedf4869db36c15f35
Ninh <phuninh0804@gmail.com>
2025-10-11 15:13:31 +0700

    Add files via upload

---
+++ Cau34.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+long long n=0;
+ cout << "Nhap so n \n"  ;
+ cin  >>  n ;
+ long double  S=0;
+ for( int i = 1 ; i <=n ; i++){
+    S +=  sqrt(i);
+ }
+
+ cout << " gia tri cua S la " << S << endl;
+  return 0;
+}
