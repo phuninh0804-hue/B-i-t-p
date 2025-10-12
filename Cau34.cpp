04bf65f8f45185287cbad2f09af46f012662fc49
Ninh <phuninh0804@gmail.com>
2025-10-12 13:45:49 +0700

    Update Cau34.cpp

--- Cau34.cpp
+++ Cau34.cpp
@@ -4,13 +4,14 @@ int main() {
 long long n=0;
  cout << "Nhap so n \n"  ;
  cin  >>  n ;
- long double  S=0;
- for( int i = 1 ; i <=n ; i++){
-    S +=  sqrt( i + S);
+ long double  S=sqrt (1.0);
+ for( int i = 2 ; i <=n ; i++){
+    S =  sqrt( i + S);
  }
 
  cout << " gia tri cua S la " << S << endl;
   return 0;
 }
+
 
 
