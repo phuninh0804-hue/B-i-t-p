1f314280ed4d37a03e57ff36ea98682339724a7d
Ninh <phuninh0804@gmail.com>
2025-10-12 13:49:23 +0700

    Update Cau33.cpp

--- Cau33.cpp
+++ Cau33.cpp
@@ -6,9 +6,10 @@ long long n=0;
  cin  >>  n ;
  long double  S=0;
  for( int i = 1 ; i <=n ; i++){
-    S += ( sqrt (2 + S));
+    S = ( sqrt (2 + S));
  }
 
  cout << " gia tri cua S la " << S << endl;
   return 0;
 }
+
