0e4fb4ddec36b0cef5e9237dc18ba090efaa0d2c
Ninh <phuninh0804@gmail.com>
2025-10-14 21:04:09 +0700

    Add files via upload

---
+++ Cau46.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+   long long n = 0 ; cout << "Nhap n: ";cin >> n;
+   long long giatribandaucuan= n ;
+   int soluongsole = 0;
+
+  while (  n!=0){
+  int chuso = n%10;
+    { if ( chuso %2 !=0 ) {
+    soluongsole++;
+    }
+
+n/=10;
+    }
+    }
+
+  cout << " so luong chu so cua le  "<< giatribandaucuan << " la " << soluongsole << endl;
+  return 0;
+}
