1b5059dd50aeb929df72da556dc42c4acf679c04
Ninh <phuninh0804@gmail.com>
2025-10-14 20:23:34 +0700

    Add files via upload

---
+++ Cau44.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+   long long n = 0 ; cout << "Nhap n: ";cin >> n;
+   long long giatribandaucuan= n ;
+   long long tong =0;
+  while (  n!=0){
+        tong += n%10;
+        n/=10;
+  }
+  cout << " so luong chu so cua "<< giatribandaucuan << " la " << tong << endl;
+  return 0;
+}
