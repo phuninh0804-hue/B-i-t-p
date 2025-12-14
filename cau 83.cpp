c037eb169748f34690bc7bfe0e6688485a9c9189
Ninh <phuninh0804@gmail.com>
2025-12-14 13:03:15 +0700

    Add files via upload

---
+++ cau 83.cpp
@@ -0,0 +1,13 @@
+ #include <bits/stdc++.h>
+ using namespace std;
+ int main (){
+ int a =0 ; cout << "Nhap so a \n" ; cin >> a ;
+ int b =0 ; cout << " nhap so b \n" ; cin >>b;
+ if ( (a > 0 && b < 0 ) || (a < 0 && b >0 )){
+    cout<< "Hai so nay trai dau" << endl;
+ }
+  else{
+    cout << "Hai so nay cung dau ";
+  }
+ return 0 ;
+ }
