7a3e5dc1491d6a792fec30e0a8ecc351d05e09e2
Ninh <phuninh0804@gmail.com>
2026-01-11 17:48:05 +0700

    Add files via upload

---
+++ cau 107.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+
+int n = 0; cout << " nhap n " ; cin >> n;
+double x = 0 ; cout << " nhap x "; cin >> x;
+double S=0;
+
+double s= pow( x, 1.0/n );
+cout <<" gia tri cua s la "<<setprecision(4) << s <<endl;
+}
---
+++ cau 108.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+
+int y = 0; cout << " nhap y " ; cin >> y;
+double x = 0 ; cout << " nhap x "; cin >> x;
+double S=0;
+
+double s= pow( x, y );
+cout <<" gia tri cua s la " << s <<endl;
+}
